adacee61185917f660f93aa74fb119b4988d65f4
Magnus Madsen <Magnusbg@outlook.dk>
2026-01-15 11:06:40 +0100

    Update struktur

--- M-kode_DeepFace/Main.py
+++ M-kode_DeepFace/Main.py
@@ -1,25 +1,41 @@
-import os
-import time
-import uuid
-import cv2
+import tensorflow as tf
+import json
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus: 
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+tf.config.list_physical_devices('GPU') 
+
+images = tf.data.Dataset.list_files('data/images/*.jpg')
+
+images.as_numpy_iterator().next()
+
+def load_image(x): 
+    byte_img = tf.io.read_file(x)
+    img = tf.io.decode_jpeg(byte_img)
+    return img
+
+images = images.map(load_image)
+
+images.as_numpy_iterator().next()
+
+type(images)
 
 
-IMAGES_PATH = os.path.join('data','Images')
-number_images = 30
+image_generator = images.batch(4).as_numpy_iterator()
 
-cap = cv2.VideoCapture(0)
-for imgnum in range(number_images):
-    print('Collecting image {}'.format(imgnum))
-    ret, frame = cap.read()
-    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
-    cv2.imwrite(imgname, frame)
-    cv2.imshow('frame', frame)
-    time.sleep(0.5)
+plot_images = image_generator.next()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx, image in enumerate(plot_images):
+    ax[idx].imshow(image) 
+plt.show()
+
+
 
 
 
